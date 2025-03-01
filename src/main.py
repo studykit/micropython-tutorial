960984b64e74f7efbc7a9b42f3d3feff44cb0ef0
studykit <studykit.ai@gmail.com>
2025-03-01 15:31:23 +0900

    Display Time

--- src/main.py
+++ src/main.py
@@ -12,6 +12,7 @@ clock.setup()
 # OLED 디스플레이 설정 (I2C)
 i2c = I2C(scl=Pin(12), sda=Pin(14))
 display = ssd1306.SSD1306_I2C(128, 64, i2c)
+display.fill(0)
 
 writer = Writer(display, fonts, False)
 writer.set_clip(True, True, False)
@@ -30,31 +31,31 @@ def read_sensor():
 
 tick = True
 while True:
-    # 센서에서 온도와 습도 읽기
-    temperature, humidity = read_sensor()
-    
-    # OLED 디스플레이 초기화
-    display.fill(0)
-    writer.set_textpos(display, 0, 0)
     h, m = clock.now()
-
     if h > 12:
         h -= 12
         am_pm = "오후"
     else:
         am_pm = "오전"
 
-    if tick:
-        writer.printstring(f"{am_pm} {h:02d}:{m:02d}\n")
-    else:
-        writer.printstring(f"{am_pm} {h:02d} {m:02d}\n")
+    writer.set_textpos(display, 0, 0)
 
+    if tick: 
+        # 센서에서 온도와 습도 읽기
+        temperature, humidity = read_sensor()
+        fmt = f"{am_pm} {h:02d}:{m:02d}\n"
+        writer.printstring(fmt)
+        writer.printstring(f'습도   {humidity}%\n')
+        writer.printstring(f"온도   {temperature}°\n")
+    else:
+        fmt = f"{am_pm} {h:02d} {m:02d}\n"
+        writer.printstring(fmt)
+
+    # OLED 디스플레이 초기화
     tick = not tick
-    writer.printstring(f'습도   {humidity}%\n')
-    writer.printstring(f"온도   {temperature}°\n")
 
     # 디스플레이 업데이트
     display.show()
     
     # 2초 대기
-    time.sleep(2)
+    time.sleep(1)
